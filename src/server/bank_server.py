b80c031a3b5cdfb651c9488bb2ed7b3eac8711b9
cnguyen-dev <cnguyen-dev@cnguyen-dev.myguest.virtualbox.org>
2026-05-17 15:53:44 -0700

    Intial Commit

---
+++ src/server/bank_server.py
@@ -0,0 +1,16 @@
+import cryptography
+import socket
+
+class BankServer:
+        
+        def __init__(self):
+            pass
+        def start(self):
+            pass
+        def stop(self):
+            pass
+        def handle_client(self,connection):
+            pass
+
+                
+        
